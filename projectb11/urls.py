b51d42c3c2c86347bf89a1923c3b2116bd129136
Robert Owens <raowwens15@icloud.com>
2021-03-14 09:58:37 -0400

    Added the functional google auth with confirmation on screen

--- projectb11/urls.py
+++ projectb11/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="exercise_gamification/index.html")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', RedirectView.as_view(url='/')),
 ]
