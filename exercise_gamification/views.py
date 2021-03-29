de02043ce156adcba2fffef022ee76c52e0f6827
Parth Raut <parthlr321@gmail.com>
2021-03-29 15:15:55 -0400

    User must be logged in to submit/view workouts

--- exercise_gamification/views.py
+++ exercise_gamification/views.py
@@ -18,92 +18,108 @@ def display_workouts(request):
             p = Profile.objects.get(user=request.user)
         workouts = p.workouts.all()
         return render(request, 'exercise_gamification/workouts.html', {'workouts': workouts})
-    HttpResponseRedirect('/home/')
+    return HttpResponseRedirect('/accounts/login')
 
 
 def choose_workout_type(request):
-    return render(request, 'exercise_gamification/choose_workout.html')
+    if (request.user.is_authenticated):
+        return render(request, 'exercise_gamification/choose_workout.html')
+    return HttpResponseRedirect('/accounts/login')
 
 def load_cardio_subimssion(request):
-    return render(request, 'exercise_gamification/cardio_form.html')
+    if (request.user.is_authenticated):
+        return render(request, 'exercise_gamification/cardio_form.html')
+    return HttpResponseRedirect('/accounts/login')
 
 def load_strength_submission(request):
-    return render(request, 'exercise_gamification/strength_form.html')
+    if (request.user.is_authenticated):
+        return render(request, 'exercise_gamification/strength_form.html')
+    return HttpResponseRedirect('/accounts/login')
 
 def load_other_submission(request):
-    return render(request, 'exercise_gamification/other_workout_form.html')
+    if (request.user.is_authenticated):
+        return render(request, 'exercise_gamification/other_workout_form.html')
+    return HttpResponseRedirect('/accounts/login')
 
 def redirect_workout(request):
-    workout_type = request.POST.getlist('workout_type')
-    if ('cardio' in workout_type):
-        return HttpResponseRedirect(reverse('exercise_gamification:cardio_workout'))
-    elif ('strength' in workout_type):
-        return HttpResponseRedirect(reverse('exercise_gamification:strength_workout'))
-    elif ('other' in workout_type):
-        return HttpResponseRedirect(reverse('exercise_gamification:other_workout'))
-    return HttpResponseRedirect(reverse('exercise_gamification:choose_workout'))
+    if (request.user.is_authenticated):
+        workout_type = request.POST.getlist('workout_type')
+        if ('cardio' in workout_type):
+            return HttpResponseRedirect(reverse('exercise_gamification:cardio_workout'))
+        elif ('strength' in workout_type):
+            return HttpResponseRedirect(reverse('exercise_gamification:strength_workout'))
+        elif ('other' in workout_type):
+            return HttpResponseRedirect(reverse('exercise_gamification:other_workout'))
+        return HttpResponseRedirect(reverse('exercise_gamification:choose_workout'))
+    return HttpResponseRedirect('/accounts/login')
 
 def submit_cardio_workout(request):
-    p = Profile.objects.get(user=request.user)
+    if (request.user.is_authenticated):
+        p = Profile.objects.get(user=request.user)
 
-    workout_name = request.POST['workout_name']
-    calories = 300 # Dummy value for now
-    workout_notes = request.POST['notes']
+        workout_name = request.POST['workout_name']
+        calories = 300 # Dummy value for now
+        workout_notes = request.POST['notes']
 
-    cardio_duration = request.POST['duration']
-    cardio_distance = request.POST['distance']
+        cardio_duration = request.POST['duration']
+        cardio_distance = request.POST['distance']
 
-    w = Workout(base_profile=p, date=timezone.now(), name=workout_name, estimated_calories=calories, notes=workout_notes)
-    w.save()
-    c = Cardio_Workout(workout=w, duration=cardio_duration, distance=cardio_distance)
-    c.save()
+        w = Workout(base_profile=p, date=timezone.now(), name=workout_name, estimated_calories=calories, notes=workout_notes)
+        w.save()
+        c = Cardio_Workout(workout=w, duration=cardio_duration, distance=cardio_distance)
+        c.save()
 
-    p.workouts.add(w)
+        p.workouts.add(w)
 
-    return HttpResponseRedirect(reverse('exercise_gamification:workouts'))
+        return HttpResponseRedirect(reverse('exercise_gamification:workouts'))
+    return HttpResponseRedirect('/accounts/login')
 
 def submit_strength_workout(request):
-    p = Profile.objects.get(user=request.user)
+    if (request.user.is_authenticated):
+        p = Profile.objects.get(user=request.user)
 
-    workout_name = request.POST['workout_name']
-    calories = 300 # Dummy value for now
-    workout_notes = request.POST['notes']
+        workout_name = request.POST['workout_name']
+        calories = 300 # Dummy value for now
+        workout_notes = request.POST['notes']
 
-    strength_bodyweight = request.POST.getlist('bodyweight')
-    strength_repitions = request.POST['repetitions']
-    strength_weight = request.POST['weight']
+        strength_bodyweight = request.POST.getlist('bodyweight')
+        strength_repitions = request.POST['repetitions']
+        strength_weight = request.POST['weight']
 
-    is_bodyweight = False
+        is_bodyweight = False
 
-    if ('on' in strength_bodyweight):
-        strength_weight = 0
-        is_bodyweight = True
+        if ('on' in strength_bodyweight):
+            strength_weight = 0
+            is_bodyweight = True
 
-    w = Workout(base_profile=p, date=timezone.now(), name=workout_name, estimated_calories=calories, notes=workout_notes)
-    w.save()
+        w = Workout(base_profile=p, date=timezone.now(), name=workout_name, estimated_calories=calories, notes=workout_notes)
+        w.save()
 
-    s = Strength_Workout(workout=w, bodyweight=is_bodyweight, repetitions=strength_repitions, weight=strength_weight)
-    s.save()
+        s = Strength_Workout(workout=w, bodyweight=is_bodyweight, repetitions=strength_repitions, weight=strength_weight)
+        s.save()
 
-    p.workouts.add(w)
+        p.workouts.add(w)
 
-    return HttpResponseRedirect(reverse('exercise_gamification:workouts'))
+        return HttpResponseRedirect(reverse('exercise_gamification:workouts'))
+    return HttpResponseRedirect('/accounts/login')
 
 def submit_other_workout(request):
-    p = Profile.objects.get(user=request.user)
+    if (request.user.is_authenticated):
+        p = Profile.objects.get(user=request.user)
 
-    workout_name = request.POST['workout_name']
-    calories = 300 # Dummy value for now
-    workout_notes = request.POST['notes']
+        workout_name = request.POST['workout_name']
+        calories = 300 # Dummy value for now
+        workout_notes = request.POST['notes']
 
-    workout_description = request.POST['description']
+        workout_description = request.POST['description']
 
-    w = Workout(base_profile=p, date=timezone.now(), name=workout_name, estimated_calories=calories, notes=workout_notes)
-    w.save()
+        w = Workout(base_profile=p, date=timezone.now(), name=workout_name, estimated_calories=calories, notes=workout_notes)
+        w.save()
 
-    o = Other_Workout(workout=w, description=workout_description)
-    o.save()
+        o = Other_Workout(workout=w, description=workout_description)
+        o.save()
 
-    p.workouts.add(w)
+        p.workouts.add(w)
 
-    return HttpResponseRedirect(reverse('exercise_gamification:workouts'))
+        return HttpResponseRedirect(reverse('exercise_gamification:workouts'))
+    return HttpResponseRedirect('/accounts/login')
