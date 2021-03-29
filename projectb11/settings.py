89eed4f8c765181c444372e3bf53f3d887a20b99
Parth Raut <parthlr321@gmail.com>
2021-03-29 14:47:40 -0400

    Changed settings.py back for heroku

--- projectb11/settings.py
+++ projectb11/settings.py
@@ -93,13 +93,8 @@ WSGI_APPLICATION = 'projectb11.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 #Required for Google Auth
 AUTHENTICATION_BACKENDS = [
