b4c581578fb2804ebc5df03b3278b63af15d8da0
Teddy Oline <teddyo28@yahoo.com>
2021-05-04 00:57:50 -0400

    Properly configured databases for all uses.

--- exercise_gamification/tests.py
+++ exercise_gamification/tests.py
@@ -1,9 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile, Workout, Cardio_Workout, Strength_Workout, Other_Workout
 
+class ExerciseTestCase(TestCase):
 
-class DummyTestCase(TestCase):
+    def setUp(self):
+        User.objects.create()
+        u = User.objects.get()
+        Profile.objects.create(user=u, name='John', username='johnstudent', bio="I am a student", level=0, xp=0)
+
     
-    def test_dummy(self):
-        s = 'hello world'
-        self.assertEqual(s, 'hello world')
+    def test_profile_creation(self):
+        john = Profile.objects.get(name='John')
+        self.assertEqual(john.username, 'johnstudent')
+        self.assertEqual(john.bio, 'I am a student')
 
+
+    
