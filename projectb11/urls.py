69ab9a2b5a12d4ea8af18869f970789895a99232
Robert Owens <raowwens15@icloud.com>
2021-05-06 10:26:22 -0400

    DIrectly to google signin does not work. Reverting to orginal

--- projectb11/urls.py
+++ projectb11/urls.py
@@ -22,7 +22,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path(r'b11adminpath/', admin.site.urls),
-    path('accounts/login', RedirectView('accounts/google/login/?process=login'))
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', RedirectView.as_view(url='/')),
     path('exercise_gamification/', include('exercise_gamification.urls')),
